a143b54855c3bcc8b84846df4d6796b84b1e9695
askabdulhaseeb <askabdulhaseeb@gmail.com>
2022-08-22 16:34:32 +0500

    add custom widgets

---
+++ lib/widgets/custom_widgets/custom_network_image.dart
@@ -0,0 +1,26 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
+class CustomNetworkImage extends StatelessWidget {
+  const CustomNetworkImage({
+    required this.imageURL,
+    this.fit = BoxFit.cover,
+    this.timeLimit = const Duration(days: 2),
+    Key? key,
+  }) : super(key: key);
+  final String imageURL;
+  final BoxFit? fit;
+  final Duration? timeLimit;
+
+  @override
+  Widget build(BuildContext context) {
+    return CachedNetworkImage(
+      imageUrl: imageURL,
+      fit: fit,
+      placeholder: (BuildContext context, String url) =>
+          const CircularProgressIndicator(),
+      errorWidget: (BuildContext context, String url, _) =>
+          const Icon(Icons.error),
+    );
+  }
+}
