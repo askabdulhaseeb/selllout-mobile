eaec680b600620f229595ba2551ef83156db73a0
Abdul Haseeb <62133331+askabdulhaseeb@users.noreply.github.com>
2022-12-09 04:52:52 +0500

    contact update

--- lib/screens/setting_screens/contact_screen.dart
+++ lib/screens/setting_screens/contact_screen.dart
@@ -37,48 +37,14 @@ class _ContactScreenState extends State<ContactScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final TextEditingController _search = TextEditingController();
+    String search = '';
     return Scaffold(
       appBar: AppBar(title: const Text('Contacts')),
       body: FutureBuilder<List<Contact>>(
         future: FastContacts.allContacts,
         builder: (BuildContext context, AsyncSnapshot<List<Contact>> snapshot) {
           if (snapshot.hasData) {
-            final List<Contact> all = snapshot.data ?? <Contact>[];
-            return Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: Consumer<UserProvider>(
-                  builder: (BuildContext context, UserProvider userPro, _) {
-                return Column(
-                  children: <Widget>[
-                    CustomTextFormField(
-                      controller: _search,
-                      hint: 'Search contact',
-                      validator: (String? value) => null,
-                    ),
-                    Expanded(
-                      child: ListView.builder(
-                        itemCount: all.length,
-                        itemBuilder: (BuildContext context, int index) {
-                          final AppUser? user =
-                              userPro.userByPhone(value: all[index].phones[0]);
-                          return user == null
-                              ? ListTile(
-                                  dense: true,
-                                  contentPadding: const EdgeInsets.all(0),
-                                  leading: const Icon(Icons.contacts_outlined),
-                                  title: Text(all[index].displayName),
-                                  subtitle: Text(all[index].phones[0]),
-                                )
-                              : _AppUserContact(
-                                  user: user, contact: all[index]);
-                        },
-                      ),
-                    ),
-                  ],
-                );
-              }),
-            );
+            return _DisplayContacts(contacts: snapshot.data ?? <Contact>[]);
           } else {
             return snapshot.hasError
                 ? const Text('Error while fetching')
@@ -86,6 +52,82 @@ class _ContactScreenState extends State<ContactScreen> {
           }
         },
       ),
+    );
+  }
+}
+
+class _DisplayContacts extends StatefulWidget {
+  const _DisplayContacts({required this.contacts, super.key});
+  final List<Contact> contacts;
+
+  @override
+  State<_DisplayContacts> createState() => __DisplayContactsState();
+}
+
+class __DisplayContactsState extends State<_DisplayContacts> {
+  String search = '';
+  List<Contact> filtered() {
+    return widget.contacts
+        .where((Contact element) => element.displayName.contains(search))
+        .toList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Consumer<UserProvider>(
+          builder: (BuildContext context, UserProvider userPro, _) {
+        return Column(
+          children: <Widget>[
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 6),
+              padding: const EdgeInsets.symmetric(horizontal: 12),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(14),
+                color: Theme.of(context)
+                    .textTheme
+                    .bodyText1!
+                    .color!
+                    .withOpacity(0.15),
+                border: Border.all(color: Colors.grey),
+              ),
+              child: TextFormField(
+                onChanged: (String value) => setState(() {
+                  search = value;
+                }),
+                cursorColor: Theme.of(context).colorScheme.secondary,
+                decoration: InputDecoration(
+                  hintText: 'Search Contact',
+                  focusColor: Theme.of(context).primaryColor,
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+            Expanded(
+              child: filtered().isEmpty
+                  ? const Center(child: Text('No Contacts available'))
+                  : ListView.builder(
+                      itemCount: filtered().length,
+                      itemBuilder: (BuildContext context, int index) {
+                        final AppUser? user = userPro.userByPhone(
+                            value: filtered()[index].phones[0]);
+                        return user == null
+                            ? ListTile(
+                                dense: true,
+                                contentPadding: const EdgeInsets.all(0),
+                                leading: const Icon(Icons.contacts_outlined),
+                                title: Text(filtered()[index].displayName),
+                                subtitle: Text(filtered()[index].phones[0]),
+                              )
+                            : _AppUserContact(
+                                user: user, contact: filtered()[index]);
+                      },
+                    ),
+            ),
+          ],
+        );
+      }),
     );
   }
 }
